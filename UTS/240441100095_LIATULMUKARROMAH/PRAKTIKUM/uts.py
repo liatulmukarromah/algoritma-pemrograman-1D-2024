79e0210ca0f70eb9214a5905f659e3ed635383b6
liatulmukarromah <liatulmukarromah@gmail.com>
2024-10-16 16:49:29 +0700

    240441100095_LiatulMukarromah_UTS

---
+++ UTS/240441100095_LIATULMUKARROMAH/PRAKTIKUM/uts.py
@@ -0,0 +1,28 @@
+#kamu harus makan
+makan =(input("sudah makan?: "))
+mandi =(input("sudah mandi?: "))
+pilihan_transportasi =(input("menggunakan motor: "))
+
+print(f"{pilihan_transportasi} total waktu perjalanan 30 menit")
+print(f"kamu berangkat tepat waktu")
+
+makan =(input("sudah makan?: "))
+mandi =(input("sudah mandi?: "))
+pilihan_transportasi =(input("menggunakan sepeda: "))
+
+print(f"{pilihan_transportasi} total waktu persiapan perjalanan 50 menit")
+print(f"kamu berangkat tepat waktu")
+
+makan =(input("sudah makan?: "))
+mandi =(input("sudah mandi?: "))
+pilihan_transportasi =(input("jalan kaki: "))
+
+print(f"{pilihan_transportasi} total waktu persiapan perjalanan 40 menit")
+print(f"kamu berangkat tepat waktu")
+
+makan =(input("sudah makan?: "))
+mandi =(input("sudah mandi?: "))
+pilihan_transportasi =(input(" sepeda: "))
+
+print(f"{pilihan_transportasi} total waktu persiapan perjalanan 65 menit")
+print(f"kamu terlambat")
