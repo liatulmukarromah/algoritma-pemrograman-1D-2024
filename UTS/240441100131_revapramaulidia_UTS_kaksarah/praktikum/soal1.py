5b322aaa3a6b3a6344b2752175cf3dfffbe88d12
revaprama <pramaulidiareva@gmail.com>
2024-10-23 22:41:34 +0700

    240441100131_revapramaulidia_UTS_kaksarah

---
+++ UTS/240441100131_revapramaulidia_UTS_kaksarah/praktikum/soal1.py
@@ -0,0 +1,16 @@
+# makan=str(input("apakah yang kamu lakukan: "))
+# print(makan)
+# if("apakah sudah makan?(sudah/belum):"):
+
+# elif("jika belum kmu punya waktu 15 untuk makan")
+
+makan=str(input ("apakah kamu sudah makan: (sudah/belum:)"):
+mandi=str(input("apakah kamu sudah mandi (ya/belum:)"):
+         print("(ya/belum:)")
+transportasi= "jalan kaki" or "motor"
+if("motor/jalan kaki:"):
+     print
+if ("apakah kamu sudah makan:(sudah/belum)"):
+elif("apakah kamu sudah mandi:(sudah/belum)"):
+transportasi = "sepeda"
+
