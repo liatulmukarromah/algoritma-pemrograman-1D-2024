ec20fb6ee9cb551460ac5b1c8286e76e32fe1081
revaprama <pramaulidiareva@gmail.com>
2024-10-23 22:27:26 +0700

    240441100131_revapramaulidia_modul1_kaksarah

---
+++ modul 1/240441100131_revapramaulidia_modul1_kaksarah/nomer 2.py
@@ -0,0 +1,14 @@
+# Mencari suku ke-8
+print("a + 4b = 11 ") # pers1
+print("2a + 18b = 52 ") # pers2
+# substitusi pers 1 ke 2
+# setelah itu kita mengetahui nilai b= 3
+# lalu kita substtusi b = 3 ke pers 1 sehingga kita dapat nilai a = -1
+
+a = -1
+b = 3
+n = 8
+
+hasil = n/2*(2*a + (n-1)*b)
+print(hasil)
+
---
+++ modul 1/240441100131_revapramaulidia_modul1_kaksarah/nomer3.py
@@ -0,0 +1,9 @@
+# jumlah dolar suraji 
+usd = 35
+#kurs usd ke idr tanggal 25 septepber 2024
+usd_ke_idr = 15124
+
+#konversi usd ke idr 
+usd_to_idr = usd * usd_ke_idr
+print("nilai uang suraji jika di konversi ke rupiah adalah Rp" ,usd_to_idr)
+
