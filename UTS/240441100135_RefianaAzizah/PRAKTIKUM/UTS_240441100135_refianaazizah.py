942fbbf3cea0939126e9d07c9479f1b808c7a881
refianaazizah <refianaazizah01@gmail.com>
2024-10-23 19:45:44 -0700

    240441100135_RefianaAzizah_UTS_AsprakKakSarah

---
+++ UTS/240441100135_RefianaAzizah/PRAKTIKUM/UTS_240441100135_refianaazizah.py
@@ -0,0 +1,12 @@
+print ("sudah makan")
+print ("sudah")
+if :
+    print("sudah makan")
+else :
+      "belum"print (input("sudah"))
+print (input)"iya"))
+
+print (input("sudah mandi"))
+print (input("belum mandi"))
+
+
