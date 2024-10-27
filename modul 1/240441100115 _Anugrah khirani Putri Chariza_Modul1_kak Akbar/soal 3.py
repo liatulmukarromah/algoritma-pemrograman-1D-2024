c728300961197aacc075b087a813de9c0da1ed77
anugrahkhirani <anugrahkhirani050521@gmail.com>
2024-10-27 09:27:33 +0700

    240441100115_AnugrahKhiraniPutriChariza_Modul1_AsprakKakAkbar (#172)

---
+++ modul 1/240441100115 _Anugrah khirani Putri Chariza_Modul1_kak Akbar/soal 3.py
@@ -0,0 +1,18 @@
+<<<<<<< HEAD
+# jumlah dolar suraji 
+usd = 35
+#kurs usd ke idr tanggal 25 september 2024
+usd_ke_idr = 15125
+
+#konversi usd ke idr 
+usd_to_idr = usd * usd_ke_idr
+=======
+# jumlah dolar suraji 
+usd = 35
+#kurs usd ke idr tanggal 25 september 2024
+usd_ke_idr = 15125
+
+#konversi usd ke idr 
+usd_to_idr = usd * usd_ke_idr
+>>>>>>> 5f1a2b6ffe2b446db620c3c89b6912c38591c60d
+print("nilai uang suraji jika di konversi ke rupiah adalah Rp" ,usd_to_idr)
---
+++ modul 1/240441100115 _Anugrah khirani Putri Chariza_Modul1_kak Akbar/soal 4.py
@@ -0,0 +1,28 @@
+<<<<<<< HEAD
+#total tim
+n = 7
+#jumlah orang
+r = 4
+#faktorial dari 7
+faktorial_7 = 7*6*5*4*3*2*1
+#faktorial dari 4
+faktorial_4 = 4*3*2*1
+#faktorial dari (7-4) adalah 3
+faktorial_3 = 3*2*1
+#perhitungan jumlah 
+jumlah_cara = (faktorial_7)/(faktorial_4*faktorial_3)
+=======
+#total tim
+n = 7
+#jumlah orang
+r = 4
+#faktorial dari 7
+faktorial_7 = 7*6*5*4*3*2*1
+#faktorial dari 4
+faktorial_4 = 4*3*2*1
+#faktorial dari (7-4) adalah 3
+faktorial_3 = 3*2*1
+#perhitungan jumlah 
+jumlah_cara = (faktorial_7)/(faktorial_4*faktorial_3)
+>>>>>>> 5f1a2b6ffe2b446db620c3c89b6912c38591c60d
+print(jumlah_cara)
