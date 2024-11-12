858bd437cab1d8f022614f19ec91325149ce37d5
liatulmukarromah <liatulmukarromah@gmail.com>
2024-11-12 01:37:30 -0800

    240441100095_LiatulMukarromah_Modul4_AsprakKakAkbar

---
+++ modul 4/240441100095_LiatulMukarromah/tugas2 (1).py
@@ -0,0 +1,8 @@
+AngkaBulat = input("masukkan bilangan bulat yang akan diurut secara terbalik :")
+AngkaTerbalik = ""
+
+for digit in AngkaBulat:
+    AngkaTerbalik = digit 
+
+print(f"bilangan diurutkan secara terbalik : {AngkaTerbalik}")
+ 
